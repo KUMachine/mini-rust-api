bde3f981309f66ebbf3089dd5bd51a7e344cef76
KUMachine <areyan.naaman@dit.gov.krd>
2025-06-10 10:35:07 +0300

    Adds tokio and axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(async || "Hello, World!"));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
+        .await
+        .unwrap();
+    println!("Axum server running on http://127.0.0.1:4000");
+    axum::serve(listener, app).await.unwrap();
 }
