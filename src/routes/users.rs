666193c1204271e754298698e1b52d98f921baf9
KUMachine <areyan.naaman@dit.gov.krd>
2025-06-11 21:30:42 +0300

    Adds validators to users routes

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,5 +1,5 @@
-use crate::models::user::{CreateUser, UserResponse};
-use crate::response::{ ApiResponse, ApiErrorResponse};
+use crate::models::user::{UserResponse};
+use crate::response::{ ApiResponse};
 use crate::errors::{AppError, AppResult};
 
 use axum::{
@@ -8,17 +8,18 @@ use axum::{
     Json, Router,
 };
 use sea_orm::{EntityTrait, ActiveModelTrait, Set};
-
+use validator::Validate;
 use crate::entity::users;
 use crate::entity::users::Entity as Users;
 use crate::AppState;
+use crate::validators::user::CreateUserRequest;
 
 pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/users", get(list_users).post(create_user))
 }
 
-async fn list_users(State(state): State<AppState>) -> AppResult<Json<ApiResponse<Vec<UserResponse>>>> {
+pub async fn list_users(State(state): State<AppState>) -> AppResult<Json<ApiResponse<Vec<UserResponse>>>> {
     let users = Users::find()
         .all(&*state.db)
         .await?;
@@ -26,10 +27,11 @@ async fn list_users(State(state): State<AppState>) -> AppResult<Json<ApiResponse
     Ok(Json(ApiResponse::ok(users.into_iter().map(UserResponse::from).collect())))
 }
 
-async fn create_user(
+pub async fn create_user(
     State(state): State<AppState>,
-    Json(payload): Json<CreateUser>,
+    Json(payload): Json<CreateUserRequest>,
 ) -> AppResult<Json<ApiResponse<UserResponse>>> {
+    payload.validate().map_err(|e| AppError::ValidationError(e.to_string()))?;
     let new_user = users::ActiveModel {
         first_name: Set(payload.first_name),
         last_name: Set(payload.last_name),
