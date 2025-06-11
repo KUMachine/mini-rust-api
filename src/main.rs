5c2b59bc51132628e5ad2ab677c1de0ed789b4fd
KUMachine <areyan.naaman@dit.gov.krd>
2025-06-11 13:44:21 +0300

    Integrates all of it to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
-use axum::{Router, routing::get};
+mod db;
+mod entity;
+mod config;
+mod routes;
+mod models;
+mod response;
+mod errors;
+
+use axum::{
+    Router,
+    routing::{get},
+};
+use sea_orm::{ActiveModelTrait, DbConn, EntityTrait};
+use serde::{Deserialize, Serialize};
+use std::{sync::Arc};
+use crate::config::Config;
+
+#[derive(Clone)]
+struct AppState {
+    db: Arc<DbConn>,
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(async || "Hello, World!"));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
-        .await
-        .unwrap();
-    println!("Axum server running on http://127.0.0.1:4000");
+    let config = Config::from_env();
+
+    let db = Arc::new(
+        db::connection::connect()
+            .await
+            .expect("Failed to connect to database"),
+    );
+
+    let state = AppState { db };
+
+    let app = Router::new()
+        .merge( routes::users::routes())
+        .route("/health", get(|| async { "Okay!" }))
+        .with_state(state);
+
+    let address = format!("{}:{}", config.server_host, config.server_port);
+
+    let listener = tokio::net::TcpListener::bind(&address).await.expect("Failed to bind the address");
+
+    println!("🚀 Server running at http://{}", address);
     axum::serve(listener, app).await.unwrap();
 }
