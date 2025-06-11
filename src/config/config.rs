6ecf14a4158182f3075f3f08a34a9a60eb6955f7
KUMachine <areyan.naaman@dit.gov.krd>
2025-06-11 13:41:08 +0300

    Isolates config.rs for envs and other server configurations

---
+++ src/config/config.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub database_url: String,
+    pub server_host: String,
+    pub server_port: u16,
+}
+
+impl Config {
+    pub fn from_env() -> Self {
+        dotenvy::dotenv().ok();
+        let database_url = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let server_host = dotenvy::var("SERVER_HOST").expect("SERVER_HOST must be set");
+        let server_port = dotenvy::var("SERVER_PORT").unwrap().parse().expect("SERVER_PORT must be set");
+
+        Self {
+            database_url,
+            server_host,
+            server_port,
+        }
+    }
+}
